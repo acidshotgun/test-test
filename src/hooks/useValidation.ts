08c0aa0d157b645fca2ce7e0a45712f3885c293c
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2024-03-19 14:13:32 +0300

    update numbers

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -7,21 +7,21 @@ const useCustomValidation = () => {
   const validateData = (lineNumber: number, key: string, value: number) => {
     switch (key) {
       case "speed":
-        if (!Number.isInteger(value) || value < 0) {
+        if (value < 0) {
           dispatch(addErrors(`${lineNumber}_${key}`));
         } else {
           dispatch(removeError(`${lineNumber}_${key}`));
         }
         break;
       case "force":
-        if (typeof value !== "number" || value <= 0) {
+        if (value <= 0 || value % 1 == 0) {
           dispatch(addErrors(`${lineNumber}_${key}`));
         } else {
           dispatch(removeError(`${lineNumber}_${key}`));
         }
         break;
       case "engineAmperage":
-        if (!Number.isInteger(value) || value <= 0) {
+        if (value <= 0) {
           dispatch(addErrors(`${lineNumber}_${key}`));
         } else {
           dispatch(removeError(`${lineNumber}_${key}`));
